6b784640e36d534f169d2db24a2780aba29e920d
Vijendra2016 <vsisodiyadesign@gmail.com>
2025-07-29 22:59:09 +0530

    about page  added

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,21 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-300 py-16 px-4 text-center">
+      <h2 className="text-5xl md:text-6xl font-serif font-semibold text-black mb-6">
+        Interested In Working Together?
+      </h2>
+      <p className="max-w-2xl mx-auto text-lg md:text-xl text-black mb-4">
+        Looking to shopify developer for  your business, just starting out, or an agency in need
+        of a part-time shopify developer ? I’d love to hear more! Drop me an email or
+        book a complimentary discovery call with me right now!
+      </p>
+      <a
+        href="mailto:vsisodiyadesign@gmail.com"
+        className="text-xl font-medium text-black hover:underline"
+      >
+        vsisodiyadesign@gmail.com
+
+      </a>
+    </footer>
+  );
+}
