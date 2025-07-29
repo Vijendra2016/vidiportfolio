03339e38323e3bfbec5c2864b936b9cf284cf0f0
Vijendra2016 <vsisodiyadesign@gmail.com>
2025-07-29 16:07:32 +0530

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    
+    
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -12,9 +14,15 @@ export default function Home() {
           height={38}
           priority
         />
+
+    
+
         <h1 className="text-3xl md:text-9xl text-black font-medium tracking-tight "> Vijendra Singh Sisodiya</h1>
-        Shopify plus developer 
-        
+       
+        <p className="mt-10 text-lg sm:text-xl max-w-xl font-sans text-gray-800">
+        Shopify plus Developer ,8 years experince in shopify ecosystem  <br />
+        building memorable and influential brands.
+      </p>
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
