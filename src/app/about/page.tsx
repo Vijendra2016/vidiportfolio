18556036e12e203f0af641d6f8416d0cd4cc2fdc
Vijendra2016 <vsisodiyadesign@gmail.com>
2025-07-30 11:08:15 +0530

    github feed integration

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -26,79 +26,13 @@ export default function About() {
          Shopify plus, CRO , A/B test , Next js , checkout/customer extension , Techinal SEO , corevita 
       </p>
       
-    <div className="w-full flex justify-center items-center text-black font-serif flex-wrap">
-        <h1 className="text-[12vw] leading-none text-black"> Vijendra </h1>
-        <div className="w-[100px] h-[100px] sm:w-[140px] sm:h-[140px] mx-4 rounded-lg overflow-hidden shadow-lg">
-          
-
-
-          <Image
-          className="w-full h-full object-cover"
-          src=" https://cdn.prod.website-files.com/67860b0fa33a316e96823102/6889a239262bcd5186ba0891_Vidinew.png"
-          alt="Next.js logo"
-          width={280}
-          height={458}
-          priority
-           
-        />
-        </div>
-        <h1 className="text-[12vw] leading-none text-black">Singh</h1>
-       
-      </div>
-
-
+    
        
         
         
         
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+     
     </div>
   );
 }
